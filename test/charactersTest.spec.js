e617c3f0c11acd4738a753de999c6adabe975e43
Luis <hichi8520@gmail.com>
2021-04-09 21:35:25 -0600

    Testing get character and episode

--- test/charactersTest.spec.js
+++ test/charactersTest.spec.js
@@ -5,10 +5,20 @@ const axios = require('axios')
 
 describe('async test Characters', () => {
     it('should get an specific character', () => {
-        axios.get('http://localhost:9000/api/v1/characters/605040e05e4b051144258b2e').then((res) => {
+        return axios.get('http://localhost:9000/api/v1/characters/605040e05e4b051144258b2e').then((res) => {
             expect(res.data.name).to.be.equal('Walter White')
-        }).catch((err) => {
-            
-        });
+            expect(res.data.nickname).to.be.equal('Heisenberg')
+            expect(res.data.occupation).to.be.equal('Chemistry Teacher')
+            expect(res.data.portrayed).to.be.equal('Bryan Cranston')
+        })
+    });
+
+    it('should get an specific episode', () => {
+        return axios.get('http://localhost:9000/api/v1/episodes/606f4a349685d91f24d362b9').then((res) => {
+            expect(res.data.title).to.be.equal('Pilot')
+            expect(res.data.season).to.be.equal('1')
+            expect(res.data.episode).to.be.equal('1')
+            expect(res.data.air_date).to.be.equal('20-01-2008')
+        })
     });
 });
